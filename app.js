3509b7f438da405b4c7af855daa42295c8308d4a
Unknown <jelmer.beerling@infracom.nl>
2019-03-08 14:08:09 +0100

    error

---
+++ app.js
@@ -0,0 +1,5 @@
+import {MDCRipple} from '@material/ripple';
+import {MDCDialog} from '@material/dialog';
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
